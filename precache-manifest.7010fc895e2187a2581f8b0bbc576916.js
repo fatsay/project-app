f84aaa5f6ee5a88795107e85328c6fba2758ea66
fatsay <fatih_sayilir@hotmail.com>
2020-05-16 04:09:04 +0300

    Updates

---
+++ precache-manifest.7010fc895e2187a2581f8b0bbc576916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0bf8a932d4c57ec5412b51be2c4f002",
+    "url": "/project-app/index.html"
+  },
+  {
+    "revision": "ce8f79cf0f83deb2a177",
+    "url": "/project-app/static/css/main.717a1c12.chunk.css"
+  },
+  {
+    "revision": "56bb2a69a266757f0562",
+    "url": "/project-app/static/js/2.e31fb5d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-app/static/js/2.e31fb5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8f79cf0f83deb2a177",
+    "url": "/project-app/static/js/main.fe5546e9.chunk.js"
+  },
+  {
+    "revision": "a155362c457c8051926b",
+    "url": "/project-app/static/js/runtime-main.8663afa6.js"
+  }
+]);
